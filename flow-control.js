20ec15af059f97d676f3e40fa5949902105cb541
Dean Norman <dean@22seven.com>
2017-09-07 10:04:28 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age =< 12) {
+    return 'You are a kid'
+  } else if ( age >= 20 ) {
+    return 'You are a grownup'
+  } else {
+    return 'You are a teenager'
+  }
 }
 
 function ternaryTeenager(age) {
